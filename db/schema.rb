d726f5b0f01078fd7c5f9c251b6d1399498c4d10
JL <jorge.rubio.rev@gmail.com>
2019-10-22 11:28:33 -0500

    before startin admin feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_18_052914) do
+ActiveRecord::Schema.define(version: 2019_10_18_200229) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "Name"
     t.string "foodreq"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
