2f5c284f85979022a65b67c61a5b1b4f89018068
JL <jorge.rubio.rev@gmail.com>
2019-11-10 03:45:45 -0600

    restyle of whole site, added favicon, newlogo, navbar, buttons and titles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -63,14 +63,13 @@ class UsersController < ApplicationController
 
 
 
-
   private
     def set_user 
       @user = User.find(params[:id])
     end
     
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :foodreq, :name)
+      params.require(:user).permit(:email, :password, :password_confirmation, :foodreq, :name, :file)
     end
 
     
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'home#index'
   
   resources :users
+  resources :upload
   resources :sessions, only: [:new, :create, :destroy]
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
