6bba1962a389e5d66f00028d40765f55e8e045b7
JL <jorge.rubio.rev@gmail.com>
2019-11-12 12:28:43 -0600

    cleaning code

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get 'menutoday', to: 'dinning#today', as: 'menutoday'
   get 'frequest', to: 'dinning#frequest', as: 'frequest'
   get 'rules', to: 'info#rules', as: 'rules'
+  
 
 
   get '/users/:id/passwordchange', to:'users#passwordchange', as: 'passwordchange'
